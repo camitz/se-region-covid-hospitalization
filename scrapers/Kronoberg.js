b26f379ab780275ac74622bd4dd0cd8048fb6eff
Martin Camitz <martin.camitz@gmail.com>
2021-04-22 20:31:14 +0200

    kronoberg bugged

--- scrapers/Kronoberg.js
+++ scrapers/Kronoberg.js
@@ -3,14 +3,14 @@ const $ = require('jquery');
 const moment = require('../moment.js')
 
 class Kronoberg extends Scraper{
-    _baseUrl = 'http://www.regionkronoberg.se/corona';
+    _baseUrl = 'https://regionkronoberg.se/corona';
 
 	get name() {
     return 'Kronoberg';
   }
 
   parse(xmlDoc){
-        var table = xmlDoc.evaluate('//*[@id="content"]/div[1]/div', xmlDoc).iterateNext();
+        var table = xmlDoc.evaluate('//*[@id="content"]/div[2]/div', xmlDoc).iterateNext();
         var t = table.querySelectorAll('table td')[4].innerText;
         var raw = table.innerText.substr(0,300);
 

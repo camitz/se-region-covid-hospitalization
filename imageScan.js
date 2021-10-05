7f3ae151256a3d4ae9db6d5526b5e9bbc45e6dc9
Martin Camitz <martin.camitz@cocoin.com>
2021-10-05 15:12:17 +0200

    Made a lot of progress.

--- imageScan.js
+++ imageScan.js
@@ -2,61 +2,87 @@ const ImageWrapper = require('./imageWrapper.js');
 const PCA = require('pca-js');
 
 class ImageScan{
-	constructor(img){
+	constructor(img, settings){
 		this._img = img;
+		this._settings = settings;
 		this.generateScan();
 	}
 
-	get canvas(){
-		return this._canvas;
+	get verticalScan(){
+		return this._verticalScan;
+	}
+
+	get horizontalScan(){
+		return this._horizontalScan;
+	}
+
+	get colorTable(){
+		return this._colorTable16;
 	}
 
 	generateScan() {
 		let img = this._img;
 		const bitdepth = 4;
 
-		var verticalScan = new Array(this.width);
+		var verticalScan = new Array(this.width), horizontalScan = new Array(this.height)
 		var colors = [];
 		var colorHist = [];
 
+		if (this._settings?.colorTable) {
+			colors = this._settings.colorTable.map(x=>x[0]).map(x => parseInt("0x"+x));
+			colorHist = new Array(colors.length).fill(0);
+		}
+
+
+		for(var y=0; y <img.height;y++)
+			horizontalScan[y] = new Array(this.width);
+
 		for(var x=0; x<img.width ;x++){
-			verticalScan[x] = new Array(this.heigth);
+			verticalScan[x] = new Array(this.height);
 
 		    for(var y=0; y <img.height;y++){
-				var offset = (img.height-y-1) * img.width*bitdepth + x*bitdepth; //Offset into image data, flipped y-axis.
+				var offset = ((img.height-y-1) * img.width + x) * bitdepth; //Offset into image data, flipped y-axis.
 				
 				var pixel = img.imageData.data[offset]*256*256 + img.imageData.data[offset+1]*256 + img.imageData.data[offset+2]; //24 bit color value
 
 				verticalScan[x][y] = pixel;
+				horizontalScan[y][x] = pixel;
 
 				//Build color list table and histogram
-				if(colors.indexOf(verticalScan[x][y]) === - 1){ 
+				if(colors.indexOf(pixel) === - 1) {
 					colors.push(verticalScan[x][y]);
 					colorHist.push(0);
 				}
 
-				colorHist[colors.indexOf(verticalScan[x][y])]++;
+				colorHist[colors.indexOf(pixel)]++;
 		    }
 		}
 
-		var colorTable = colors.map((x,i)=>[x.toString(16), colorHist[i]]).sort((a,b)=>b[1]-a[1]); //Build ordered mapping into histogram of hex-strings.
-		colors = colorTable.map(x => parseInt("0x"+x[0]));  //Order colors accordingly.
+		var colorTable = colors.map((x,i)=>[x.toString(16).padStart(6,"0"), colorHist[i]]);//Build ordered mapping into histogram of hex-strings.
+		if (!this._settings?.colorTable) { //Sort by frequency, only if colortable not provided.
+			colorTable = colorTable.sort((a,b)=>b[1]-a[1]);
+			colors = colorTable.map(x => parseInt("0x"+x[0]));  //Order colors accordingly.
+		}
 
 		for(x=0; x<img.width; x++){
 		    for(y=0; y <img.height; y++){
 				verticalScan[x][y] = colors.indexOf(verticalScan[x][y]); //Map scan into colortable.
+				horizontalScan[y][x] = colors.indexOf(horizontalScan[y][x]);
 		    }
 		}
 
 		//pca
 		var pcaData = colorTable.map(x => [parseInt("0x"+x[0][0]+x[0][1]), parseInt("0x"+x[0][2]+x[0][3]), parseInt("0x"+x[0][4]+x[0][5])]);
 		var vectors = PCA.getEigenVectors(pcaData);
-		var adData = PCA.computeAdjustedData(pcaData, vectors[0])
+		var adData = PCA.computeAdjustedData(pcaData, vectors[0]);
+		var norm = [Math.min(adData.adjustedData[0]), Math.max(adData.adjustedData[0])];
+
+		this._colorTablePCA = adData.adjustedData[0].map((k, i) => [k/(norm[1]-norm[0])+norm[0], colorTable[i][1]]);
 
 		this._verticalScan = verticalScan;
+		this._horizontalScan = horizontalScan;
 		this._colors32 = colors;
 		this._colorTable16 = colorTable;
-		this._colorTablePCA = adData[0];
 	}
 
 	_colorHist = [];
--- scrapers/Gotland.js
+++ scrapers/Gotland.js
@@ -16,11 +16,10 @@ class Gotland extends Scraper{
   parse(xmlDoc){
        var t=xmlDoc.evaluate("//*[@id='contentContainer']//a[contains(text(),'Veckorapport om covid')]", xmlDoc).iterateNext();
        var h = t.href;
-               
+
        var t = new GotlandSub(h);
 
-       return t.scrape();
-
+       return t.scrape().then(r=>{r[4] = h; return r;});
   }
 }
 
@@ -31,7 +30,7 @@ class GotlandSub extends Scraper{
 	_baseUrl = 'https://www.gotland.se/';
 
 	get name() {
-        return 'Uppsala';
+        return 'Gotland';
   }
 
   contenttype = "pdf";
@@ -66,6 +65,9 @@ class GotlandSub extends Scraper{
   }
   
   parse(img){
+	var legendImg = img.portion(220,49,50,200);
+	legendImg.putToDom(document.getElementById("gotlandgraph"));
+	var legendScan = new ImageScan(legendImg);
 
 	var graphDim = [177, 480, 700, 300]; //originX, originY, graphWidth, graphHeight
 
@@ -78,104 +80,57 @@ class GotlandSub extends Scraper{
 	var graphImg = img.portion(...graphDim);
 	graphImg.putToDom(document.getElementById("gotlandgraph"));
 
-	xaxisImg.rotate(-Math.PI/4).putToDom(document.getElementById("gotlandgraph"));
 
-	var scan = new ImageScan(graphImg);
+	var graphScan = new ImageScan(graphImg, {colorTable: legendScan.colorTable});
 
-	return Tesseract.recognize(
-	  xaxisImg.rotate(-Math.PI/4).canvas,
-	  'eng',
-	  { logger: m => console.log(m) }
-	).then(result => {
-	  console.log(result);
-	});
+	var yaxisScan = new ImageScan(yaxisImg);
+	var yTickPos = yaxisScan.verticalScan[47].reduce((a,e,i)=>{if(e>0)a.push(i);return a;},[]);
+	
+	var xaxisScan = new ImageScan(xaxisImg);
+	var xTickPos = xaxisScan.horizontalScan[xaxisImg.height-6].reduce((a,e,i)=>{if(e>0 && (a.length==0 || a[a.length-1]<i-5))a.push(i);return a;},[]);
 
-	var verticalScan = [], colors=[],colorHist=[];
-  	var bitdepth = img.data.length/img.width/img.height;
 
-	var canvas = document.getElementById("uppsalacanvas");
-	this.ctx = canvas.getContext("2d");
-	this.zoom = img.width<1000 ? 2 : 1;
+    document.getElementById("verticalScan").innerText=graphScan.verticalScan.map((x,i)=>""+i+": "+x.map(y=>y>=10?(y+""):("0"+y)).join(",")).join("\n");
 
-	canvas.width = img.width * this.zoom;
-	canvas.height = img.height * this.zoom;
+	var extractData = function(ocrResults){
+		console.log(graphScan);
+		this.dates = ocrResults[0].map(d=>moment(d));
 
-  	//verticalScan=verticalScan.splice(41);
-    
-    document.getElementById("verticalScan").innerText=verticalScan.map((x,i)=>""+i+": "+x.map(y=>y>=10?(y+""):("0"+y)).join(",")).join("\n");
-    const colorInl = 2,//bitdepth==4?2:1, //apricot,"fe01ff"1
-        colorIva = 8,//,//red, "f3a4eb"4/5
-        colorOther = 6;//3; //greyblue,"9189aa"2
-        
-    const firstBarStartsAt = verticalScan.findIndex(v=>v.some(x=>x==colorIva||x==colorOther||x==colorInl));
-    for (var v = firstBarStartsAt; v < firstBarStartsAt+20 && verticalScan[v+1].filter(x=>x==colorIva||x==colorOther||x==colorInl).length == verticalScan[v].filter(x=>x==colorIva||x==colorOther||x==colorInl).length; v++);
-    for (v = v+1; v < firstBarStartsAt+20 && verticalScan[v+1].filter(x=>x==colorIva||x==colorOther||x==colorInl).length == verticalScan[v].filter(x=>x==colorIva||x==colorOther||x==colorInl).length; v++);
+		var maxYLabel = parseInt(ocrResults[1][0].text)
+		var maxLabeledTickYPos = yTickPos.map(y=>Math.abs(y-yaxisImg.height+ocrResults[1][0].ypos));
+		maxLabeledTickYPos  = maxLabeledTickYPos.lastIndexOf(Math.min(...maxLabeledTickYPos));
+		maxLabeledTickYPos = yTickPos[maxLabeledTickYPos];
+		
+		var barScans = xTickPos.map(x=>graphScan.verticalScan[x].reduce((a,e,i) => {
+			switch(e){
+				case 0: a[0] += 1.0/maxLabeledTickYPos*maxYLabel; break;
+			}
+			return a;
+		},[0,0,0,0]));
+		
+        return [this.dates[0],0,0,null,this.url];
+	}
 
-    const maxBarAndGapWidth=v+2-firstBarStartsAt;
-
-    var bars=[],d=0,vs=[];
-    for (v = firstBarStartsAt; v < img.width; v += maxBarAndGapWidth) {
-    	var v0=v;
-    	while(v>v0-3 && verticalScan[v-1].filter(x=>x==colorIva||x==colorOther||x==colorInl).length == verticalScan[v].filter(x=>x==colorIva||x==colorOther||x==colorInl).length)
-    	    v--;
-    	vs.push(v);
-    	var sample = verticalScan[v].slice(Math.floor(50/600*img.height));
-    	var bar = [sample.filter(x=>x==colorInl).length,
-    	        sample.filter(x=>x==colorIva).length,
-    	        sample.filter(x=>x==colorOther).length];
-    	bars.push(bar);
-        
-
-        var o = sample.reduceRight((a,x,i)=>(x==colorIva||x==colorOther||x==colorInl)&&a==-1?i:a, -1);
-        this.plot(v, o);
-        this.plot(v, o-=bar[0]);
-        this.plot(v, o-=bar[1]);
-        this.plot(v, o-=bar[2]);
-    }
-
-    bars = bars.map(b=>
-    	    [...b,
-    	    b.reduce((a,x)=>a+x),
-    	    0//b.reduce((a,x)=>a+(x>0?1:0),0)-1 //space in between bars
-    	    ]);
-    bars = bars.map(b=>
-    	    [...b,
-    	    b[3]+b[4]
-    	    ]);
-    	    
-    const maxBar = bars.map(b=>b[5]).reduce((a,b)=>Math.max(a,b));
-    bars = bars.map(b=>
-    	    [...b,
-    	    b[5]/maxBar*127.0+1,
-    	    b[1]/maxBar*127.0,
-    	    ]);
-
-        var date = this.url.match(/\d{6}/);
-        date = moment(date,"YYMMDD");
-        var dates = [...Array(bars.length).keys()].map(x=>moment(date).add(-bars.length+1+x,'days'));
-        var inls = bars.map(x=>Math.round(x[6])), ivas =bars.map(x=>Math.round(x[7]));
-        
-    bars = bars.map((b,i) => [vs[i],...b]);
-
-        [this.dates,this.inls,this.ivas]=[dates,inls,ivas];
-
-        return [date,this.inls[this.inls.length-1],this.ivas[this.inls.length-1],null,this.url,null,
-        this.dates,
-        this.inls,
-        this.ivas
-        ];
-  }
-
-    plot(x,y){
-  	if(!x && !Y)
-  	return;
-  	  this.ctx.beginPath();
-      this.ctx.arc(x*this.zoom,y*this.zoom, 2, 0, 2 * Math.PI, false);
-      this.ctx.fillStyle = 'pink';
-      this.ctx.fill();
-//      context.lineWidth = 5;
-  //    context.strokeStyle = '#003300';
-    //  context.stroke();
+	return Promise.all([
+			Tesseract.recognize(
+			  xaxisImg.rotate(-Math.PI/4).canvas,
+			  'eng',
+			  { logger: m => console.log(m) }
+			).then(result => {
+			  console.log(result);
+			  return result.data.lines.map(x=>x.text.replace("\n",""));
+			}),
+			Tesseract.recognize(
+			  yaxisImg.portion(0,0,43,graphDim[3]).canvas,
+			  'eng',
+			  { logger: m => console.log(m) }
+			).then(result => {
+			  console.log(result);
+			  return result.data.lines.map(x=>{return {text:x.text.replace("\n",""),ypos:(x.bbox.y0+x.bbox.y1)/2};});
+			})]).then(((extractData, parser, results)=>{
+				console.log(results.length);
+				extractData.bind(parser)(results);
+			}).bind(undefined, extractData, this));
   }
 
 }

634e7329b6850c0cb44b71185ab1eba998f53d81
Martin Camitz <martin.camitz@cocoin.com>
2021-12-05 22:47:44 +0100

    Corrected Halland.

--- scrapers/Halland.js
+++ scrapers/Halland.js
@@ -30,7 +30,7 @@ class Halland extends Scraper{
 		var inl = xmlDoc.evaluate('//*[@id="main"]/article/table[2]/tbody/tr[2]/td[2]',xmlDoc,null,XPathResult.STRING_TYPE ).stringValue;
         inl=inl.match(/\d+/)[0]*1;
 
-        var iva = xmlDoc.evaluate('//*[@id="main"]/article/table[2]/tbody/tr[2]/td[2]',xmlDoc,null,XPathResult.STRING_TYPE ).stringValue;        
+        var iva = xmlDoc.evaluate('//*[@id="main"]/article/table[2]/tbody/tr[3]/td[2]',xmlDoc,null,XPathResult.STRING_TYPE ).stringValue;        
         iva=iva.match(/\d+/)[0]*1;
         
         var raw = xmlDoc.evaluate('//*[@id="main"]/article/table[1]',xmlDoc,null,XPathResult.STRING_TYPE ).stringValue;

1f3fcbfd0020d40b1d936bfae1c894046e121b80
Martin Camitz <martin.camitz@cocoin.com>
2022-02-16 14:20:12 +0100

    Fixed Blekinge

--- scrapers/Blekinge.js
+++ scrapers/Blekinge.js
@@ -4,25 +4,24 @@ const moment = require('../moment.js')
 
 
 class Blekinge extends Scraper{
-    _baseUrl = 'https://regionblekinge.se/halsa-och-vard/for-vardgivare/smittskyddsenheten/information-om-coronaviruset.html';
+    _baseUrl = 'https://regionblekinge.se/halsa-och-vard/for-vardgivare/smittskyddsenheten/covid-19-aktuellt-lage-i-region-blekinge.html';
 
 	get name() {
     return 'Blekinge';
   }
 
   parse(xmlDoc) {
-        var raw = xmlDoc.evaluate('//*[@id="svid12_2cd827be170f3b15ca913afb"]/div[2]//table', xmlDoc).iterateNext();
+        var raw = xmlDoc.evaluate('//*[@id="svid12_205b7bc717dc2b0f42df36f2"]/div[2]//table', xmlDoc).iterateNext();
         raw = raw.innerText;
 
-        var node = xmlDoc.evaluate('//*[@id="svid12_2cd827be170f3b15ca913afb"]/div[2]//table/tbody/tr/td', xmlDoc);
-        var t = node.iterateNext();
+        var node = xmlDoc.evaluate('//*[@id="svid12_205b7bc717dc2b0f42df36f2"]/div[2]//table/tbody/tr[2]', xmlDoc);
         var t = node.iterateNext();
 
-        var inl = t.innerText.match(/\d+/)[0]*1;
-        var iva = t.innerText.match(/\((\d+)/)[1];
+        var inl = t.children[1].innerText.match(/\d+/)[0]*1;
+        var iva = t.children[1].innerText.match(/\((\d+)/)[1];
 
-        var date = xmlDoc.evaluate('//*[@id="svid12_2cd827be170f3b15ca913af1"]//h2', xmlDoc).iterateNext().innerText.match(/den (\d{1,2} \w+)/)[1];
-        date = moment(date);
+        var date = moment(t.children[0].innerText);
+        date.set("year",moment().get("year"))
 
         return [date,inl,iva,raw];
   }
